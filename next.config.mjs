efa97713f39a13058c061bcc9db8f4d08e359673
dzodze00 <pakpablie@gmail.com>
2025-03-18 00:13:02 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,38 +1,8 @@
-let userConfig = undefined
-try {
-  userConfig = await import('./v0-user-next.config')
-} catch (e) {
-  // ignore error
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // Make sure there are no experimental features or redirects that might interfere with routing
+  // No experimental features
 };
 
-mergeConfig(nextConfig, userConfig)
-
-function mergeConfig(nextConfig, userConfig) {
-  if (!userConfig) {
-    return
-  }
-
-  for (const key in userConfig) {
-    if (
-      typeof nextConfig[key] === 'object' &&
-      !Array.isArray(nextConfig[key])
-    ) {
-      nextConfig[key] = {
-        ...nextConfig[key],
-        ...userConfig[key],
-      }
-    } else {
-      nextConfig[key] = userConfig[key]
-    }
-  }
-}
-
-export default nextConfig
-
+export default nextConfig;
